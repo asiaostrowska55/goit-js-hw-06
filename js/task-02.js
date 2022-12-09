e0aac4cf168a7f37d7b4911ad2efb330e0bbbb3d
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2022-12-09 21:45:47 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const item = document.querySelector("#ingredients");
+
+const listElement = ingredients.forEach((el) => {
+  let list = document.createElement("li");
+  list.textContent = el;
+  item.append(list);
+});
+
+console.log(item);
