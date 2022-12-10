b78d7ce4a23db90170f143189f35e62e75cc8b0f
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2022-12-10 23:22:17 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("All fields needs to be filled!");
+  }
+  console.log(`Email: ${email.value}\nPassword: ${password.value}`);
+  form.reset();
+}
